587c71af54595ccb4b49f29d753f3cb2cf061473
mshenhera <mshenhera@users.noreply.github.com>
2019-10-08 16:02:22 +0300

    Create GitHubMultibranch.groovy

---
+++ utilities/GitHubMultibranch.groovy
@@ -0,0 +1,42 @@
+package utilities
+ 
+import javaposse.jobdsl.dsl.DslFactory
+ 
+class GithubMultibranch {
+ 
+    String name
+    String description
+    String displayName
+    String repositoryOwner
+    String repositoryName
+    String credentials
+    String includeBranches
+    String excludeBranches
+ 
+ 
+    void build(DslFactory dslFactory) {
+        def job = dslFactory.multibranchPipelineJob(name) {
+            description(description)
+            displayName(displayName)
+            branchSources {
+                github {
+                    scanCredentialsId(credentials)
+                    repoOwner(repositoryOwner)
+                    repository(repositoryName)
+                    includes(includeBranches)
+                    excludes(excludeBranches)
+                }
+            }
+            factory {
+                workflowBranchProjectFactory {
+                    scriptPath('Jenkinsfile')
+                }
+            }
+            orphanedItemStrategy {
+                discardOldItems {
+                    numToKeep(15)
+                }
+            }
+        }
+    }
+}
